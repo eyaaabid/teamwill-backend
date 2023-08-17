0d2ec20f132e42259e2d6e4db35e6d8b69fb65e1
Eya Abid <eyaabid60@gmail.com>
2023-08-17 22:27:35 +0100

    commit

---
+++ src/api/popular-training/controllers/popular-training.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * popular-training controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::popular-training.popular-training');
---
+++ src/api/popular-training/routes/popular-training.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * popular-training router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::popular-training.popular-training');
